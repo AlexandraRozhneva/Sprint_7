cc4268aa358705a11f265a7be70cc736ad7f78ad
Alexandra Rozhneva <wolchica27@yandex.ru>
2026-07-11 10:54:42 +0300

    test_courier_creation.py

--- tests/test_courier_creation.py
+++ tests/test_courier_creation.py
@@ -2,107 +2,49 @@ import pytest
 import allure
 import requests
 from api.courier_api import CourierAPI
-from api.helpers import generate_random_string
+from constants.urls import Urls
+from constants.messages import ErrorMessages, SuccessMessages
+from helpers.data_generators import generate_random_login, generate_random_password, generate_random_first_name
 
 
 @allure.feature('Создание курьера')
 class TestCourierCreation:
     
     @allure.title('Курьера можно создать')
-    def test_create_courier_success(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
+    @pytest.mark.parametrize('has_first_name', [True, False])
+    def test_create_courier_success(self, has_first_name):
+        login = generate_random_login()
+        password = generate_random_password()
+        first_name = generate_random_first_name() if has_first_name else None
         
         response = CourierAPI.create_courier(login, password, first_name)
         
         assert response.status_code == 201
-        assert response.json() == {"ok": True}
+        assert response.json() == SuccessMessages.OK_TRUE
     
     @allure.title('Нельзя создать двух одинаковых курьеров')
     def test_create_duplicate_courier(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
+        login = generate_random_login()
+        password = generate_random_password()
+        first_name = generate_random_first_name()
         
-        # Первый запрос
         response1 = CourierAPI.create_courier(login, password, first_name)
         assert response1.status_code == 201
         
-        # Второй запрос с теми же данными
         response2 = CourierAPI.create_courier(login, password, first_name)
         assert response2.status_code == 409
-        assert "Этот логин уже используется" in response2.text
+        assert ErrorMessages.LOGIN_ALREADY_USED in response2.text
     
-    @allure.title('Для создания курьера нужно передать все обязательные поля')
+    @allure.title('При создании курьера обязательны поля login и password')
     @pytest.mark.parametrize('missing_field', ['login', 'password'])
-    def test_create_courier_missing_field(self, missing_field):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
+    def test_create_courier_missing_fields(self, missing_field):
+        login = generate_random_login()
+        password = generate_random_password()
         
         payload = {"login": login, "password": password}
-        if missing_field == 'login':
-            payload.pop('login')
-        else:
-            payload.pop('password')
+        payload.pop(missing_field)
         
-        response = requests.post(
-            'https://qa-scooter.praktikum-services.ru/api/v1/courier',
-            data=payload
-        )
+        response = requests.post(Urls.get_courier_url(), data=payload)
         
         assert response.status_code == 400
-        assert "Недостаточно данных" in response.text
-    
-    @allure.title('Запрос возвращает правильный код ответа при успешном создании')
-    def test_create_courier_success_status_code(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        
-        response = CourierAPI.create_courier(login, password)
-        
-        assert response.status_code == 201
-    
-    @allure.title('Успешный запрос возвращает ok true')
-    def test_create_courier_success_response(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        
-        response = CourierAPI.create_courier(login, password)
-        
-        assert response.json() == {"ok": True}
-    
-    @allure.title('При отсутствии одного из полей возвращается ошибка')
-    @pytest.mark.parametrize('missing_field', ['login', 'password'])
-    def test_create_courier_field_required(self, missing_field):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        
-        payload = {"login": login, "password": password}
-        if missing_field == 'login':
-            payload.pop('login')
-        else:
-            payload.pop('password')
-        
-        response = requests.post(
-            'https://qa-scooter.praktikum-services.ru/api/v1/courier',
-            data=payload
-        )
-        
-        assert response.status_code == 400
-        assert "Недостаточно данных" in response.text
-    
-    @allure.title('Создание пользователя с уже существующим логином возвращает ошибку')
-    def test_create_courier_existing_login(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
-        
-        # Создаем первого курьера
-        response1 = CourierAPI.create_courier(login, password, first_name)
-        assert response1.status_code == 201
-        
-        # Пытаемся создать второго с тем же логином
-        response2 = CourierAPI.create_courier(login, generate_random_string(10))
-        assert response2.status_code == 409
-        assert "Этот логин уже используется" in response2.text
+        assert ErrorMessages.NOT_ENOUGH_DATA in response.text
