000bdd9f82788d476412f8ac2f912c0f905e4331
Alexandra Rozhneva <wolchica27@yandex.ru>
2026-07-11 11:27:21 +0300

    test_courier_login.py

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -14,23 +14,19 @@ class TestCourierLogin:
     @allure.title('Курьер может авторизоваться')
     def test_courier_login_success(self):
         courier_data = register_new_courier_and_return_login_password()
-        if courier_data:
-            login, password, first_name = courier_data
-            
-            response = CourierAPI.login_courier(login, password)
-            
-            assert response.status_code == 200
-            assert 'id' in response.json()
+        login, password, first_name = courier_data
+        
+        response = CourierAPI.login_courier(login, password)
+        
+        assert response.status_code == 200
+        assert 'id' in response.json()
     
     @allure.title('Для авторизации нужны все обязательные поля')
-    @pytest.mark.parametrize('missing_field', ['login', 'password'])
-    def test_login_missing_fields(self, missing_field):
-        login = generate_random_login()
-        password = generate_random_password()
-        
-        payload = {"login": login, "password": password}
-        payload.pop(missing_field)
-        
+    @pytest.mark.parametrize('empty_field, payload', [
+        ('login', {"login": "", "password": generate_random_password()}),
+        ('password', {"login": generate_random_login(), "password": ""})
+    ])
+    def test_login_empty_fields(self, empty_field, payload):
         response = requests.post(Urls.get_courier_login_url(), data=payload)
         
         assert response.status_code == 400
@@ -49,13 +45,12 @@ class TestCourierLogin:
     @allure.title('Неправильный пароль возвращает ошибку')
     def test_login_wrong_password(self):
         courier_data = register_new_courier_and_return_login_password()
-        if courier_data:
-            login, password, first_name = courier_data
-            
-            response = CourierAPI.login_courier(login, password + "wrong")
-            
-            assert response.status_code == 404
-            assert ErrorMessages.ACCOUNT_NOT_FOUND in response.text
+        login, password, first_name = courier_data
+        
+        response = CourierAPI.login_courier(login, password + "wrong")
+        
+        assert response.status_code == 404
+        assert ErrorMessages.ACCOUNT_NOT_FOUND in response.text
     
     @allure.title('Авторизация несуществующего пользователя возвращает ошибку')
     def test_login_nonexistent_user(self):
