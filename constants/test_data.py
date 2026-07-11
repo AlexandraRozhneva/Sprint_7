2bfe87f5d76b9ce4e2129034a9e6523585c953ad
Alexandra Rozhneva <wolchica27@yandex.ru>
2026-07-11 10:53:02 +0300

    test_data.py

---
+++ constants/test_data.py
@@ -0,0 +1,14 @@
+class OrderTestData:
+    DEFAULT_ADDRESS = "г. Москва, ул. Тестовая, д. 1"
+    DEFAULT_METRO_STATION = 1
+    DEFAULT_PHONE = "+7 999 999 99 99"
+    DEFAULT_RENT_TIME = 1
+    DEFAULT_DELIVERY_DATE = "2026-07-15"
+    DEFAULT_COMMENT = "Тестовый заказ"
+    
+    COLORS = {
+        'BLACK': ['BLACK'],
+        'GREY': ['GREY'],
+        'BOTH': ['BLACK', 'GREY'],
+        'NONE': None
+    }
