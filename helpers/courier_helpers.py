48485352089f7a22248d067e2dee7cd7da59b7a4
Alexandra Rozhneva <wolchica27@yandex.ru>
2026-07-12 10:13:49 +0300

    courier_helpers.py

--- helpers/courier_helpers.py
+++ helpers/courier_helpers.py
@@ -8,8 +8,6 @@ def register_new_courier_and_return_login_password():
     Регистрирует нового курьера и возвращает список [логин, пароль, имя]
     Если регистрация не удалась, возвращает пустой список
     """
-    login_pass = []
-    
     login = generate_random_login()
     password = generate_random_password()
     first_name = generate_random_first_name()
@@ -23,8 +21,6 @@ def register_new_courier_and_return_login_password():
     response = requests.post(Urls.get_courier_url(), data=payload)
     
     if response.status_code == 201:
-        login_pass.append(login)
-        login_pass.append(password)
-        login_pass.append(first_name)
+        return [login, password, first_name]
     
-    return login_pass
+    return []
